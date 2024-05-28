9057021a3e3ebd16ecdf56f24e8d748804a6e6bb
Claudio Ferraro <claudio@justbetter.nl>
2024-05-28 17:04:17 +0200

    Init order reminder

--- src/OrderReminderServiceProvider.php
+++ src/OrderReminderServiceProvider.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Rapidez\OrderReminder;
 
 use Illuminate\Support\ServiceProvider;
 
-class SkeletonServiceProvider extends ServiceProvider
+class OrderReminderServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/rapidez-:package_slug_without_prefix.php', 'rapidez-:package_slug_without_prefix');
+        $this->mergeConfigFrom(__DIR__.'/../config/rapidez-order-reminder.php', 'rapidez-order-reminder');
     }
 
     public function boot()
@@ -30,7 +30,7 @@ class SkeletonServiceProvider extends ServiceProvider
 
     public function bootViews() : self
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rapidez-:package_slug_without_prefix');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'rapidez-order-reminder');
 
         return $this;
     }
@@ -38,12 +38,12 @@ class SkeletonServiceProvider extends ServiceProvider
     public function bootPublishables() : self
     {
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/rapidez-:package_slug_without_prefix'),
-        ], 'rapidez-:package_slug_without_prefix-views');
+            __DIR__.'/../resources/views' => resource_path('views/vendor/rapidez-order-reminder'),
+        ], 'rapidez-order-reminder-views');
 
         $this->publishes([
-            __DIR__.'/../config/rapidez-:package_slug_without_prefix.php' => config_path('rapidez-:package_slug_without_prefix.php'),
-        ], 'rapidez-:package_slug_without_prefix-config');
+            __DIR__.'/../config/rapidez-order-reminder.php' => config_path('rapidez-order-reminder.php'),
+        ], 'rapidez-order-reminder-config');
 
         return $this;
     }
